a0e967e19441d4182833512236b72709cae51435
kimchieater <jasonj19276@gmail.com>
2024-05-07 19:37:39 +1000

    updated

--- blog-website/app/nav/page.tsx
+++ blog-website/app/nav/page.tsx
@@ -8,7 +8,16 @@ export default function Nav(){
 
   return(
     <div className="nav">
-      
+      <nav className="xs:max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl mx-auto flex justify-between">
+        <div>
+          <h3 className="">Jason Codes</h3>
+        </div>
+        <ul className="flex gap-5 ">
+          <li><a>Home</a></li>
+          <li><a>Posts</a></li>
+          <li><a>Contact</a></li>
+        </ul>
+      </nav>
 
     </div>
   )
